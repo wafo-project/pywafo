205f94d0f864abf81b98b56b5683682704ff4e1d
Per.Andreas.Brodtkorb <Per.Andreas.Brodtkorb@96a810ac-173e-11df-b9d6-e102a254d9bb>
2010-07-15 09:54:41 +0000

    Fixed a bug in findcross in c_functions.c Recompiled binaries for Windows xp 32bit

--- pywafo/src/wafo/plotbackend.py
+++ pywafo/src/wafo/plotbackend.py
@@ -1,13 +1,13 @@
 """
     Modify this file if another plotbackend is wanted.
 """
-
+import warnings
 if False:
     try:
         from scitools import easyviz as plotbackend
         print('wafo.wafodata: plotbackend is set to scitools.easyviz')
     except:
-        print('wafo: Unable to load scitools.easyviz as plotbackend')
+        warnings.warn('wafo: Unable to load scitools.easyviz as plotbackend')
         plotbackend = None
 else:
     try:
@@ -15,5 +15,5 @@ else:
         plotbackend.interactive(True)
         print('wafo.wafodata: plotbackend is set to matplotlib.pyplot')
     except:
-        print('wafo: Unable to load matplotlib.pyplot as plotbackend')
+        warnings.warn('wafo: Unable to load matplotlib.pyplot as plotbackend')
         plotbackend = None
