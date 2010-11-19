cc45593f01e47f28441e78a2ba25024621f13a84
Per.Andreas.Brodtkorb <Per.Andreas.Brodtkorb@96a810ac-173e-11df-b9d6-e102a254d9bb>
2010-11-19 15:33:50 +0000

    Fixed Scipy-ticket #1131:  ppf for Lognormal fails on array-like 'loc' or 'scale'

--- trunk/pywafo/src/wafo/stats/misc.py
+++ trunk/pywafo/src/wafo/stats/misc.py
@@ -1,9 +1,10 @@
-from numpy import asarray, ndarray, reshape, repeat, nan, product
+from numpy import asarray, ndarray, ones, nan #, reshape, repeat,  product
 
 def valarray(shape,value=nan,typecode=None):
     """Return an array of all value.
     """
-    out = reshape(repeat([value],product(shape,axis=0),axis=0),shape)
+    #out = reshape(repeat([value],product(shape,axis=0),axis=0),shape)
+    out = ones(shape, dtype=bool) * value
     if typecode is not None:
         out = out.astype(typecode)
     if not isinstance(out, ndarray):
