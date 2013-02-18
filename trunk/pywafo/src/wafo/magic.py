a866bbb65ddbe61b38a446ac4f41b4d4d5548326
Per.Andreas.Brodtkorb <Per.Andreas.Brodtkorb@96a810ac-173e-11df-b9d6-e102a254d9bb>
2013-02-18 12:20:22 +0000

    Added magic.py

---
+++ trunk/pywafo/src/wafo/magic.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 17 13:59:12 2012
+
+@author: pab
+"""
+import numpy as np
+
+def magic(n):
+    ix = np.arange(n)+1
+    J, I = np.meshgrid(ix,ix)
+    A = np.mod(I+J-(n+3)/2,n)
+    B = np.mod(I+2*J-2,n)
+    M = n*A + B + 1
+    return M
