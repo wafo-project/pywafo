7b77a84ff5e311839874428322e5201ecde63161
davidovitch <david.verelst@gmail.com>
2015-10-26 19:56:05 +0100

    remove old packaging related scripts and configs

--- pywafo/nose_all.py
+++
@@ -1,7 +0,0 @@
-import subprocess as sub
-
-print('Please wait, this may take a while')
-
-#sub.check_call('nosetests --with-coverage --cover-package=wafo', stderr=sub.STDOUT)
-sub.check_call('nosetests', stderr=sub.STDOUT)
-print('Finished')
