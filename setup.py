16bffcc72548b8b0d938710bc3892a561d1defd4
pbrod <per.andreas.brodtkorb@gmail.com>
2015-11-29 22:52:15 +0100

    Try compile mvn extenstion

--- setup.py
+++ setup.py
@@ -45,10 +45,10 @@ def setup_package_pyscaffold():
 #                         include_dirs=['wafo/source/rind2007'])
 #    extensions.append(mreg_ext)
 
-#    mvn_ext = Extension('wafo.mvn',
-#                        sources=['wafo/source/mvn/mvn.pyf',
-#                                 'wafo/source/mvn/mvndst.f'])
-#    extensions.append(mvn_ext)
+    mvn_ext = Extension('wafo.mvn',
+                        sources=['wafo/source/mvn/mvn.pyf',
+                                 'wafo/source/mvn/mvndst.f'])
+    extensions.append(mvn_ext)
 
 #    mvnprd_ext = Extension('wafo.mvnprdmod',
 #                           sources=['wafo/source/mvnprd/mvnprd.f',
