8f867567bc245c00ebb06178940d612d12ad71ba
davidovitch <david.verelst@gmail.com>
2015-12-06 11:16:17 +0100

    change library names: lib will added as prefix automagically by distutils

--- setup.py
+++ setup.py
@@ -38,10 +38,10 @@ def setup_package_pyscaffold():
     # mvnprdmod
     lib_mvnprdmod_src = ['wafo/source/mvnprd/mvnprd.f',
                          'wafo/source/mvnprd/mvnprodcorrprb.f']
-    config.add_library('lib_mvnprdmod', sources=lib_mvnprdmod_src)
+    config.add_library('_mvnprdmod', sources=lib_mvnprdmod_src)
     config.add_extension('mvnprdmod',
                          sources=['wafo/source/mreg/cov2mmpdfreg_intfc.f'],
-                         libraries=['lib_mvnprdmod'],
+                         libraries=['_mvnprdmod'],
                          depends=(lib_mvnprdmod_src))
 
     # -------------------------------------------------------------------------
@@ -49,10 +49,10 @@ def setup_package_pyscaffold():
     lib_cov2mod_src = ['wafo/source/mreg/dsvdc.f',
                        'wafo/source/mreg/mregmodule.f',
                        'wafo/source/mreg/intfcmod.f']
-    config.add_library('lib_cov2mod', sources=lib_cov2mod_src)
+    config.add_library('_cov2mod', sources=lib_cov2mod_src)
     config.add_extension('cov2mod',
                          sources=['wafo/source/mreg/cov2mmpdfreg_intfc.f'],
-                         libraries=['lib_cov2mod'],
+                         libraries=['_cov2mod'],
                          include_dirs=['wafo/source/mreg/'],
                          depends=(lib_cov2mod_src))
 
@@ -64,10 +64,10 @@ def setup_package_pyscaffold():
                        'wafo/source/rind2007/fimod.f',
                        'wafo/source/rind2007/rindmod.f',
                        'wafo/source/rind2007/rind71mod.f']
-    config.add_library('lib_rindmod', sources=lib_rindmod_src)
+    config.add_library('_rindmod', sources=lib_rindmod_src)
     config.add_extension('rindmod',
                          sources=['wafo/source/rind2007/rind_interface.f'],
-                         libraries=['lib_rindmod'],
+                         libraries=['_rindmod'],
                          include_dirs=['wafo/source/mreg/'],
                          depends=(lib_rindmod_src))
 
