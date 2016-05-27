b7ce91334e730e9152b1602ccb7637dff6465a7a
pbrod <per.andreas.brodtkorb@gmail.com>
2016-05-27 12:41:57 +0200

    Small updates

--- wafo/integrate.py
+++ wafo/integrate.py
@@ -156,8 +156,9 @@ def romberg(fun, a, b, releps=1e-3, abseps=1e-3):
     -------
     >>> import numpy as np
     >>> [q,err] = romberg(np.sqrt,0,10,0,1e-4)
-    >>> np.allclose([q,err],
-    ...    [ 21.08185107,   6.61635466e-05])
+    >>> np.allclose(q, 21.08185107)
+    True
+    >>> err < 1e-4
     True
     '''
     h = b - a
