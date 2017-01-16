e836d566915d11c612e553c74b17118a7ddde5b3
pbrod <per.andreas.brodtkorb@gmail.com>
2017-01-16 02:19:41 +0100

    updated docstring

--- wafo/gaussian.py
+++ wafo/gaussian.py
@@ -112,7 +112,7 @@ class Rind(object):
     >>> val, err, terr = rind(Sc,m,Blo,Bup,indI, xc, nt=0)
     >>> np.allclose(val, 0.05494076, rtol=4e-2)
     True
-    >>> err[0] < 1e-3, terr[0] < 1e-7
+    >>> err[0] < 5e-3, terr[0] < 1e-7
     (True, True)
 
     Compute expectation E( X1^{+}*X2^{+} ) with random
